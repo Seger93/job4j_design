bb2339dcebb46d7eda2a30ecbfe58bd887ed8bc1
Admin <Serger9393@mail.ru>
2022-04-25 01:30:15 +0300

    4.0. File [#252491]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -13,7 +13,7 @@ public class Dir {
         }
         System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println(String.format(subfile.getName() + "-" + subfile.length() / 1024 + "Kb"));
+            System.out.println(String.format("%s - %s", subfile.getName(), subfile.length() / 1024 + "Kb"));
         }
     }
 }
