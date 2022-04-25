bb33576af4e66f35794b5d7d216943c2531c3e31
Admin <Serger9393@mail.ru>
2022-04-26 02:17:36 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,15 +9,24 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js"))
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1]))
                 .forEach(System.out::println);
+
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static boolean validate(String[] s) {
+        if (s.length != 0 && s[1].endsWith(".js")) {
+            return true;
+        }
+        throw new IllegalArgumentException("Некорректные параметры, измените настройки конфигурации");
     }
 }
 
