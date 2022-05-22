8f524930b04dac33765d0da0d806c1ce883cc68a
Admin <Serger9393@mail.ru>
2022-05-22 18:49:00 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,16 +8,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Sergei";
-        int age = 29;
-        double growth = 184.4;
-        float taskPerDay = 1.45F;
-        char lit = 'G';
-        boolean write = true;
-        byte system = 64;
-        long days = 217;
-        LOG.debug("User info name : {}, age : {}, growth : {}, taskPerDay : {},"
-                        + " lit : {}, write : {}, system : {}, days :{}",
-                name, age, growth, taskPerDay, lit, write, system, days);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
