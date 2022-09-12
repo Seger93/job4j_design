6096d7c3cc532107c041364f706af2cad09efbb8
Admin <Serger9393@mail.ru>
2022-09-12 04:42:27 +0300

    Хранилище машин [#1733]

---
+++ skripts/ Machine_storages.sql
@@ -0,0 +1,48 @@
+create table car_bodies(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table car_engines(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table car_transmissions(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table cars(
+    id serial primary key,
+    name varchar(255),
+    car_transmissions_id int references car_transmissions(id),
+    car_engines_id int references car_engines(id),
+    car_bodies_id int references car_bodies(id)
+);
+
+insert into car_bodies(name) values ('Седан'),('Хэтчбек'),('Универсал'),('Пикап'),('Купе');
+insert into car_engines(name) values ('V8'),('V6'),('V4'),('Электро'),('Дизель');
+insert into car_transmissions(name) values ('Автоматическая'),('Роботизированная'),('Механика'),('DSg');
+insert into cars(name, car_transmissions_id, car_engines_id , car_bodies_id)
+values ('Вольво', 1, 1, 2), ('Опель', null, 3, 4), ('БМВ', 3, 1, 4),('Ауди', 2, 3, null),('Киа',1, null, 3);
+
+--Вывести список всех машин и все привязанные к ним детали.
+select c.id, c.name car_name, cb.name body_name, ce.name engine_name, ct.name transmission_name
+from cars c
+left join car_bodies cb on c.car_bodies_id = cb.id
+left join car_engines ce on c.car_engines_id = ce.id
+left join car_transmissions ct on c.car_transmissions_id = ct.id
+
+--Вывести кузовы, которые не используются НИ в одной машине
+select cb.name from car_bodies cb
+left join cars c on cb.id = c.car_bodies_id where c.name is null;
+
+--Вывести двигатели, которые не используются НИ в одной машине
+select ce.name from car_engines ce
+left join cars c on ce.id = c.car_engines_id where c.name is null;
+
+--Вывести коробки передач, которые не используются НИ в одной машине
+select ct.name from car_transmissions ct
+left join cars c on ct.id = c.car_transmissions_id where c.name is null;
+
